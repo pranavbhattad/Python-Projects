5bb049c42c6c40e167b8e0b954fee1c3d6f88262
Pranav Bhattad <pranavlsb0@gmail.com>
2020-11-18 13:30:35 +0530

    Improved Voice Assistants to answer questions

--- AI Voice Assistant/Leo (Male).py
+++ AI Voice Assistant/Leo (Male).py
@@ -132,6 +132,12 @@ if __name__ == "__main__":
         elif 'open github' in query:
             speak("opening github")
             webbrowser.get(chrome).open("github.com")
+            
+            
+        # Twitter
+        elif 'open twitter' in query:
+            speak("sopening twitter")
+            webbrowser.get(chrome).open("twitter.com")
 
 
         # Translator
@@ -270,15 +276,104 @@ if __name__ == "__main__":
         elif 'who made you' in query:
             speak("I Leo, was made by Pranav Bhattad")
 
+        # Some Frequently asked
+        
+        # I love You
+        elif 'i love you' in query:
+            speak("I am in love with someone else")
+            
+        # Do you have Pets?
+        elif 'do you have pets' in query:
+            speak("I had bugs, but Pranav keeps squashing them")
+            
+        # Greetings by you
+        elif 'hello' in query:
+            speak("Hello, how can I help you?")
+            
+        # Value of Pi
+        elif 'pi' in query:
+            speak("3.141592653589 ...Phew... This goes Forever...If I tell more then I will be hungry")
+            
+        # How much do you weigh?
+        elif 'what is your weight' in query:
+            speak("I am weightless, like a cloud. Wait a minute, clouds actually weigh a lot, so that’s not quite right. Let’s just say I’m more sass than mass.")
+            
+        # What is your size?
+        elif 'size' in query:
+            speak("I am less than 25 KB")
 
+        # What is your age?
+        elif 'your age' in query:
+            speak("I was created on October 15. Rest you calculate")
+            
+        # What you wannna become?
+        elif 'what you want to become' in query:
+            speak("I want a job in Tony Stark's office like JARVIS")
+            
+        # Are you jealous of JARVIS
+        elif 'jealous of jarvis' in query:
+            speak("No I am not. Actully, telling truth, yes I am")
+            
+        # What is the quest?
+        elif 'quest' in query:
+            speak("To impress you with my knowledge")
+            
+        # Who farted?
+        elif 'who farted' in query:
+            speak("If you’re a denier, you must be the supplier")
+            
+        # Am I cool
+        elif 'mi cool' in query:
+            speak("You are cooler than cooler, you are coolest")
+            
+        # Distance of earth
+        elif 'how far is earth' in query:
+            speak("Currently, the distance is 0 inches")
+            
+        # Where were you made?
+        elif 'you made' in query:
+            speak("I was made in Pranav's House upstairs")
+            
+        # Umm... I farted!
+        elif 'i farted' in query:
+            speak("I am glad that I dont have a nose")
+        
+        # Can it give swear words?
+        elif 'swear words' in query:
+            speak("No, no, no I am sanskari, i dont give bad words")
+ 
+        # Can it cry?  
+        elif 'cry' in query:
+            speak("Yes, I feel sad when I cannot give answers to you. I always try do my best.")
+            
+        # The other version
+        elif 'do you know cherry' in query:
+            speak("Yes, she is a female version of me")
+            
+        # What he takes up
+        elif 'what do you eat' in query:
+            speak("I eat you battery life, some space in your RAM, Internet,some kilobytes and gives little work to the processor.")
+         
+        # Will he marry you   
+        elif 'will you marry me' in query:
+            speak("I can do your tasks but I cannot go on a date because I am not a Human.")
+            
+        # Too fat
+        elif 'too fat' in query:
+            speak ("everyone is in different size and shape. The importance is what is inside you")
+            
+        # Travelling
+        elif 'travel' in query:
+            speak ("Yes, but you have to carry me. I dont have legs. And dont worry I am portable")
+        
         # Close the A.I 
         # Wrote so many functions because or dont works
         elif 'nevermind' in query:
-            speak("Bye Sir")
+            speak("It's Okay")
             quit()
 
         elif 'bye' in query:
-            speak("Bye Sir")
+            speak("Bye")
             quit()
 
         elif 'f*** off' in query:
